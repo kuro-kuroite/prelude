6dbdd1b2b9120e71a5054d3344529338621985dc
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-12-07 09:34:08 +0000

    chore: yarn .babel

--- scripts/tasks/linc.js
+++ scripts/tasks/linc.js
@@ -1,20 +1,21 @@
+"use strict";
+
+var _eslint = require("../eslint");
+
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-'use strict';
+// NOTE: npm-scripts is used only development
 
-var _eslint = require('../eslint');
-
+/* eslint-disable import/no-extraneous-dependencies */
 console.log('Linting changed files...');
 
-if (
-  (0, _eslint.runESLint)({
-    onlyChanged: true,
-  })
-) {
+if ((0, _eslint.runESLint)({
+  onlyChanged: true
+})) {
   console.log('Lint passed for changed files.');
 } else {
   console.log('Lint failed for changed files.');

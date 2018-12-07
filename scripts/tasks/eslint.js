7c738d62f1b8b5e8423945ec39862185a2e4d730
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-12-07 09:33:00 +0000

    feat: rename npm script dot style bacause it only use developer

--- scripts/tasks/eslint.js
+++ scripts/tasks/eslint.js
@@ -1,24 +1,25 @@
+"use strict";
+
+var _eslint = require("../eslint");
+
 /**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-'use strict';
+// NOTE: npm-scripts is used only development
 
-var _eslint = require('../eslint');
-
+/* eslint-disable import/no-extraneous-dependencies */
 console.log('Linting all files...'); // https://circleci.com/docs/2.0/env-vars/#circleci-environment-variable-descriptions
 
 if (!process.env.CI) {
-  console.log('Hint: run `yarn linc` to only lint changed files.');
+  console.log('Hint: run `yarn .linc` to only lint changed files.');
 }
 
-if (
-  (0, _eslint.runESLint)({
-    onlyChanged: false,
-  })
-) {
+if ((0, _eslint.runESLint)({
+  onlyChanged: false
+})) {
   console.log('Lint passed.');
 } else {
   console.log('Lint failed.');

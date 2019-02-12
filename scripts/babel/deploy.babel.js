4a9b1573cef27a6af74df62931ec81ef25c861c4
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2019-02-12 19:09:24 +0900

    refactor: change outPath to argument when babel

--- scripts/babel/deploy.babel.js
+++ scripts/babel/deploy.babel.js
@@ -3,6 +3,7 @@
 import { exec } from '../shared/listChangedFiles';
 
 const filePath = process.argv[2];
-const splitCommand = `yarn babel ${filePath} --out-dir dist`.split(' ');
+const outPath = process.argv[3] || 'dist';
+const splitCommand = `yarn babel ${filePath} --out-dir ${outPath}`.split(' ');
 
 exec(splitCommand[0], splitCommand.slice(1));

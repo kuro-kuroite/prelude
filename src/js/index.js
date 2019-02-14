c44b39e6f5064dd2615f5504937d5eda288be9d1
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2019-02-14 22:01:52 +0900

    feat: add all date-fns as dateFns

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
+import * as dateFns from 'date-fns';
 import { StringBuffer, pararell } from './atoms';
 import { DateFnsTz } from './organisms';
 
 // eslint-disable-next-line import/prefer-default-export
-export { DateFnsTz, StringBuffer, pararell };
+
+// eslint-disable-next-line import/prefer-default-export
+export { dateFns, DateFnsTz, StringBuffer, pararell };

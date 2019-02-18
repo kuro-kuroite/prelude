9054202ee9eb0b645389787a8c6fe6b93151c6ff
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2019-02-18 15:26:11 +0000

    fix: prelude をライブラリとして使用するときに，fsが上手く動かないので，一時的に代用する

--- src/js/atoms/fs/promisifyAll.js
+++ src/js/atoms/fs/promisifyAll.js
@@ -1,7 +1,13 @@
-import * as Promise from 'bluebird';
+// import * as Promise from 'bluebird';
 
 import fs from 'fs';
+import * as util from 'util';
 
-const fsAsync = Promise.promisifyAll(fs);
+// HACK: npm lib の時に上手く動かないので以下で代用する
+// const fsAsync = Promise.promisifyAll(fs);
+const fsAsync = {
+  readFile: util.promisify(fs.readFile),
+  writeFile: util.promisify(fs.writeFile),
+};
 
 export default fsAsync;
